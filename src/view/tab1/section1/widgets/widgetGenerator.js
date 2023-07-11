760655fd4aeaceea7dde21f83bedd1c02f2c1925
Yair Kukielka <yair.kukielkazunzunegui@devo.com>
2023-07-11 11:13:24 +0200

    add info example

--- src/view/tab1/section1/widgets/widgetGenerator.js
+++ src/view/tab1/section1/widgets/widgetGenerator.js
@@ -12,7 +12,14 @@ export default function buildGenerator(id, sizeL, sizeM, sizeS) {
         <li>sm-${sizeS}</li>
         </ul>`);
     };
-
+    const infoContent = `
+    <ul class="list">
+      <li class="list__item">Item 1</li>
+      <li class="list__item">Item 2</li>
+    </ul>`;
+    widget.setInfo({title:"This is the info title", content: infoContent});
+    widget.setInfoQuery({ title: 'This is the query title', query: `from my.table where this=that and
+     that = this`});
     return widget;
   };
 }
